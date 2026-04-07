c7e4679c175f1193ccd8c3efb65c351d51aebc7c
Ahmed Abd El-Mgeed <abdelmgeeed1@gmail.com>
2026-04-07 16:45:09 +0200

    Initial Commit

---
+++ ToDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToDoApp
+//
+//  Created by Ahmed Abd El Mgeed Sahm on 07/04/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Ahmed Abd El Mgeed Sahm on 07/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
